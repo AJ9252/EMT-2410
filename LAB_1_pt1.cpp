c37335cf75239f935c8eca8a566982bda65d8dd3
AJ9252 <125714921+AJ9252@users.noreply.github.com>
2023-02-17 22:09:24 -0500

    Add files via upload

---
+++ LAB_1_pt1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int W=5;
+    for(int i=0; i<W;i++){
+        cout << "Hello World" << endl;
+    }
+
+return 0;   
+}
---
+++ LAB_1_pt2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int W; //Number of loops
+    cout << "Enter a number: ";
+    cin >> W;
+
+
+    for(int i=0; i<W;i++){
+        cout << "Hello World" << endl;
+    }
+
+return 0;   
+}
+
+//Part 1 display hello world 5 times
+//use user input to display hello world
